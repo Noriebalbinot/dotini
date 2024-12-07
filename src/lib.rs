4b6b3ae4bb894554467b337279bf3fb8a174d43b
norie balbinot <92960355+Noriebalbinot@users.noreply.github.com>
2024-12-07 11:07:16 -0300

    add some docs

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,18 @@ use std::{collections::HashMap, path::Path};
 pub mod file;
 pub mod getters_and_setters;
 
+/// Welcome to DotIni a simple dot.ini parser
+/// # loading values:
+///  ```rust ignore
+///     let ini = DotIni::new("path/to/file.ini").load_file().unwrap();
+///     ```
+///  # getting a value loaded:
+///  ```rust ignore
+///     let ini = DotIni::new("path/to/file.ini").load_file().unwrap();
+///     let section = String::from("mysection");
+///     let property = String::from("myproperty");
+///     let value = ini.get(section,property);
+///     ```
 #[derive(Clone)]
 pub struct DotIni<'a> {
     path: &'a Path,
